807560f8c1d8f17dfc9599eb6ba00e3cbb0d4852
Hassan <Hassan@LAPTOP-1614UIVH>
2021-08-21 19:31:27 +0200

    Initial Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController] //API Controller attribute
+    [Route("api/[controller]")] //we also need to do a route
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet] //adding the first HttpGet method
+        public async Task<ActionResult<List<Product>>>  GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return Ok(products);
+        }
+        [HttpGet("{id}")] //adding the second HttpGet method.
+        public async Task<ActionResult<Product>> GetProduct(int id) //if we need a single product or id to the URL, we'll get a single product
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext //DbContext is a class in EF.
+                                          //It is a bridge between your domain
+                                          //or entity classes and the database.
+                                          //The primary class that is responsible
+                                          //for interacting with data as objects
+                                          //DbContext.
+                                          //One of the classes is Find method
+                                          //Used to find entities inside our database
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+
+        public DbSet<Product> Products { get; set; } //Products is the name of the table we want to grab information from.
+
+
+    }
+}
