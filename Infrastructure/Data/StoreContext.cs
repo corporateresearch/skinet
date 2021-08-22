e9857b4eb6ac019bc956b1ee5db43968cd636e94
Hassan <Hassan@LAPTOP-1614UIVH>
2021-08-22 23:27:18 +0200

    End of section 3

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,28 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 using Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
 {
-    public class StoreContext : DbContext //DbContext is a class in EF.
-                                          //It is a bridge between your domain
-                                          //or entity classes and the database.
-                                          //The primary class that is responsible
-                                          //for interacting with data as objects
-                                          //DbContext.
-                                          //One of the classes is Find method
-                                          //Used to find entities inside our database
+    public class StoreContext : DbContext
     {
         public StoreContext(DbContextOptions<StoreContext> options) : base(options)
         {
         }
 
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
 
-        public DbSet<Product> Products { get; set; } //Products is the name of the table we want to grab information from.
-
-
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
