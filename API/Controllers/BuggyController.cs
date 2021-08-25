fa8ef5cb9a1bcf9a7b7bb26c04202ba9fbbddf3e
Hassan <Hassan@LAPTOP-1614UIVH>
2021-08-25 02:45:38 +0200

    Section 4.1

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,47 @@
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext _context;
+        public BuggyController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("not found")]
+        public ActionResult GetNotFoundRequest()
+        {
+            var thing = _context.Products.Find(42);
+            if (thing ==null)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+        [HttpGet("server error")]
+        public ActionResult GetServerError()
+        {
+            var thing = _context.Products.Find(42);
+            var thingToReturn = thing.ToString();
+            return Ok();
+        }
+        [HttpGet("bad request")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest();
+        }
+
+        [HttpGet("bad request/{id}")]
+        public ActionResult GetNotFoundRequest(int id)
+        {
+            return Ok();
+        }
+    }
+}
